66c044b2b7fa87b49d1a073e46e75a265ccc67a1
Charis <bluegrassgal222@gmail.com>
2021-04-27 07:58:01 -0600

    set event handlers to update a mock state and to reset on clear

--- app.js
+++ app.js
@@ -1,63 +1,46 @@
 // DOM elements
-const startBalanceInput = document.getElementById('start-balance-input');
+const currentValueInput = document.getElementById('current-value-input');
+const currentValueGo = document.getElementById('current-value-go');
 const termTextInput = document.getElementById('term-text-input');
 const termSelectInput = document.getElementById('term-select-input');
+const termGo = document.getElementById('term-go');
 const interestInput = document.getElementById('interest-input');
+const interestGo = document.getElementById('interest-go');
 const compoundsSelect = document.getElementById('compounds-select');
 const paymentInput = document.getElementById('payment-input');
-const endBalanceInput = document.getElementById('end-balance-input');
-const submitBtn = document.getElementById('submit-btn');
+const paymentGo = document.getElementById('payment-go');
+const futureValueInput = document.getElementById('future-value-input');
+const futureValueGo = document.getElementById('future-value-go');
+const clearBtn = document.getElementById('clear-btn');
 
 // calc values
-const startBalance = 0;
-const termLength = 0;
-const termPeriod = 'years';
-const interestRate = 0.0;
-const compounds = 'monthly';
-const paymentAmount = 0;
-const endBalance = 0;
+let mockState = {
+  currentValue: 0,
+  termLength: 0,
+  termPeriod: 'years',
+  interestRate: 0.0,
+  compounds: 'monthly',
+  paymentAmount: 0,
+  futureValue: 0
+}
+
+
+// link inputs and values
+
 
 // EVENT HANDLERS
 const handleUpdate = field => {
-  return e => { console.log(e.currentTarget.value); field = e.currentTarget.value; }
+  return e => mockSate[field] = e.currentTarget.value;
 };
 
-const handleSubmit = e => {
-  // check to see which field should be calculated
+const calculateCurrentValue = () => {
+  // check that the proper field are filled in - display errors
 
-  // call the appropriate method
-
-  // fill in the appropriate field
+  // calculate value and fill in field
 
   // trigger graph
 };
 
-// add event listeners
-startBalanceInput.addEventListener('input', handleUpdate('startBalance'));
-termTextInput.addEventListener('input', handleUpdate('termLength'));
-termSelectInput.addEventListener('input', handleUpdate('termPeriod'));
-interestInput.addEventListener('input', handleUpdate('interestRate'));
-compoundsSelect.addEventListener('input', handleUpdate('compounds'));
-paymentInput.addEventListener('input', handleUpdate('paymentAmount'));
-endBalanceInput.addEventListener('input', handleUpdate('endBalance'));
-
-
-
-// OTHER FUNCTIONS
-
-// animate graph
-const animateGraph = () => {
-
-};
-
-// calculations
-const calculateFinalBalance = () => {
-
-};
-
-const calculateStartingBalance = () => {
-
-};
 
 const calculateTermLength = () => {
 
@@ -67,7 +50,47 @@ const calculateInterestRate = () => {
 
 };
 
-const monthlyPayment = () => {
+const calculatePaymentAmount = () => {
+
+};
+
+const calculateFutureValue = () => {
+
+};
+
+const handleClear = e => {
+  e.preventDefault();
+
+  currentValueInput.value = '';
+  termTextInput.value = '';
+  termSelectInput.value = 'years';
+  interestInput.value = '';
+  compoundsSelect.value = 'monthly';
+  paymentInput.value = '';
+  futureValueInput.value = '';
+};
+
+// add event listeners
+currentValueInput.addEventListener('input', handleUpdate('currentValue'));
+currentValueGo.addEventListener('click', calculateCurrentValue);
+termTextInput.addEventListener('input', handleUpdate('termLength'));
+termSelectInput.addEventListener('input', handleUpdate('termPeriod'));
+termGo.addEventListener('click', calculateTermLength);
+interestInput.addEventListener('input', handleUpdate('interestRate'));
+interestGo.addEventListener('click', calculateInterestRate);
+compoundsSelect.addEventListener('input', handleUpdate('compounds'));
+paymentInput.addEventListener('input', handleUpdate('paymentAmount'));
+paymentGo.addEventListener('click', calculatePaymentAmount);
+futureValueInput.addEventListener('input', handleUpdate('futureValue'));
+futureValueGo.addEventListener('click', calculateFutureValue);
+clearBtn.addEventListener('click', handleClear);
+
+
+
+// OTHER FUNCTIONS
+
+// animate graph
+const animateGraph = () => {
 
 };
 
