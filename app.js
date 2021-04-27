1c1d44a4d07b02b26560a8ffaa1f973dd7af33cd
Charis <bluegrassgal222@gmail.com>
2021-04-27 17:12:34 -0600

    Start refactoring submit handlers

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const termGo = document.getElementById('term-go');
 const interestInput = document.getElementById('interest-input');
 const interestGo = document.getElementById('interest-go');
 const compoundsSelect = document.getElementById('compounds-select');
-const paymentInput = document.getElementById('payment-input');
-const paymentGo = document.getElementById('payment-go');
+// const paymentInput = document.getElementById('payment-input');
+// const paymentGo = document.getElementById('payment-go');
 const futureValueInput = document.getElementById('future-value-input');
 const futureValueGo = document.getElementById('future-value-go');
 const clearBtn = document.getElementById('clear-btn');
@@ -17,68 +17,43 @@ const errors = {
   presentValue: document.getElementById('present-value-error'),
   termLength: document.getElementById('term-length-error'),
   interestRate: document.getElementById('interest-error'),
-  paymentAmount: document.getElementById('payment-error'),
+  // paymentAmount: document.getElementById('payment-error'),
   futureValue: document.getElementById('future-value-error')
 }
 
-// calc values
-let mockState = {
-  presentValue: 0,
-  termLength: 0,
-  termPeriod: 'years',
-  interestRate: 0.0,
-  compoundPeriods: 0,
-  paymentAmount: 0,
-  futureValue: 0
-}
 
 // link inputs and values
+const getValuesFromInput = () => {
+  return {
+    pv: (parseInt(presentValueInput.value)).toFixed(2),
+    fv: (parseInt(futureValueInput.value)).toFixed(2),
+    i: (parseFloat(interestInput.value)).toFixed(2),
+    n: parseInt(termTextInput.value) * parseInt(termTextSelect.value),
+    pmt: (parseInt(paymentInput.value)).toFixed(2)
+  };
+}
 
 
 // EVENT HANDLERS
-const handleUpdate = field => {
-  console.log('inside handleUpdate');
-  return e => {
-    if (field === 'termPeriod') mockState[field] = e.currentTarget.value;
-    else mockState[field] = (field === 'interestRate') ? parseFloat(e.currentTarget.value) : parseInt(e.currentTarget.value);
-    
-    if (mockState[field] !== '') errors[field].classList.add('hidden'); 
-  }
-};
-
-
-/*
-FV = PV x [ 1 + (i / n) ] ^ (n x (t / 12)), where:
-
-FV = Future value of money
-PV = Present value of money
-i = interest rate
-n = number of compounding periods per year
-t = number of months
-*/
-
-// Assume a sum of $10,000 is invested for 1 year at 10% interest. 
-// Monthly Compounding: FV = $10,000 x [1 + (10% / 12)] ^ (12 x 1) = $11,047
-// Daily Compounding:   FV = $10,000 x [1 + (10% / 365)] ^ (365 x 1) = $11,052
-
 
 const calculatePresentValue = e => {
   e.preventDefault();
-  console.log('inside calculatepresentvalue')
+  
   // check that the proper field are filled in - display errors
   if (termTextInput.value === '') errors['termLength'].classList.remove('hidden');
   if (interestInput.value === '') errors['interestRate'].classList.remove('hidden');
-  if (paymentInput.value === '') errors['paymentAmount'].classList.remove('hidden');
+  // if (paymentInput.value === '') errors['paymentAmount'].classList.remove('hidden');
   if (futureValueInput.value === '') errors['futureValue'].classList.remove('hidden');
 
   // convert termLength to months and find compound periods
   if (mockState['termPeriod'] === 'years') mockState['termLength'] = mockState['termLength'] * 12;
 
   // calculate missing value
-  let denom = (1 + (mockState['interestRate'] / compoundPeriods)) ** (compoundPeriods / 12);
-  mockState['presentValue'] = numer / denom;
-  dataByMonth.push(mockState['presentValue']);
-  mockState['']
+  debugger
+  let numer = mockState['futureValue'];
+  let denom = (1 + (mockState['interestRate'] / mockState['compoundPeriods'])) ** (mockState['termLength'] * mockState['compoundPeriods'] / 12);
+  mockState['presentValue'] = (numer / denom).toFixed(2);
+  presentValueInput.value = mockState['presentValue'];
 
   // trigger graph
   animateGraph();
@@ -91,7 +66,7 @@ const calculateTermLength = e => {
   // check that the proper field are filled in - display errors
   if (presentValueInput.value === '') errors["presentValue"].classList.remove('hidden');
   if (interestInput.value === '') errors["interestRate"].classList.remove('hidden');
-  if (paymentInput.value === '') errors["paymentAmount"].classList.remove('hidden');
+  // if (paymentInput.value === '') errors["paymentAmount"].classList.remove('hidden');
   if (futureValueInput.value === '') errors["futureValue"].classList.remove('hidden');
 
   // calculate value and fill in field
@@ -106,7 +81,7 @@ const calculateInterestRate = e => {
   // check that the proper field are filled in - display errors
   if (presentValueInput.value === '') errors["presentValue"].classList.remove('hidden');
   if (termTextInput.value === '') errors["termLength"].classList.remove('hidden');
-  if (paymentInput.value === '') errors["paymentAmount"].classList.remove('hidden');
+  // if (paymentInput.value === '') errors["paymentAmount"].classList.remove('hidden');
   if (futureValueInput.value === '') errors["futureValue"].classList.remove('hidden');
 
   // calculate value and fill in field
@@ -115,20 +90,20 @@ const calculateInterestRate = e => {
   animateGraph();
 };
 
-const calculatePaymentAmount = e => {
-  e.preventDefault();
-  console.log('inside calculatepresentvalue')
-  // check that the proper field are filled in - display errors
-  if (presentValueInput.value === '') errors["presentValue"].classList.remove('hidden');
-  if (termTextInput.value === '') errors["termLength"].classList.remove('hidden');
-  if (interestInput.value === '') errors["interestRate"].classList.remove('hidden');
-  if (futureValueInput.value === '') errors["futureValue"].classList.remove('hidden');
+// const calculatePaymentAmount = e => {
+//   e.preventDefault();
+//   console.log('inside calculatepresentvalue')
+//   // check that the proper field are filled in - display errors
+//   if (presentValueInput.value === '') errors["presentValue"].classList.remove('hidden');
+//   if (termTextInput.value === '') errors["termLength"].classList.remove('hidden');
+//   if (interestInput.value === '') errors["interestRate"].classList.remove('hidden');
+//   if (futureValueInput.value === '') errors["futureValue"].classList.remove('hidden');
 
-  // calculate value and fill in field
+//   // calculate value and fill in field
 
-  // trigger graph
-  animateGraph();
-};
+//   // trigger graph
+//   animateGraph();
+// };
 
 const calculateFutureValue = e => {
   e.preventDefault();
@@ -137,7 +112,7 @@ const calculateFutureValue = e => {
   if (presentValueInput.value === '') errors["presentValue"].classList.remove('hidden');
   if (termTextInput.value === '') errors["termLength"].classList.remove('hidden');
   if (interestInput.value === '') errors["interestRate"].classList.remove('hidden');
-  if (paymentInput.value === '') errors["paymentAmount"].classList.remove('hidden');
+  // if (paymentInput.value === '') errors["paymentAmount"].classList.remove('hidden');
 
   // calculate value and fill in field
 
@@ -167,8 +142,8 @@ termGo.addEventListener('click', calculateTermLength);
 interestInput.addEventListener('input', handleUpdate('interestRate'));
 interestGo.addEventListener('click', calculateInterestRate);
 compoundsSelect.addEventListener('input', handleUpdate('compoundPeriods'));
-paymentInput.addEventListener('input', handleUpdate('paymentAmount'));
-paymentGo.addEventListener('click', calculatePaymentAmount);
+// paymentInput.addEventListener('input', handleUpdate('paymentAmount'));
+// paymentGo.addEventListener('click', calculatePaymentAmount);
 futureValueInput.addEventListener('input', handleUpdate('futureValue'));
 futureValueGo.addEventListener('click', calculateFutureValue);
 clearBtn.addEventListener('click', handleClear);
