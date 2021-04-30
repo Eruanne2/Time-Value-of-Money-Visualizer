bd539d524092c2b0db358b70672e95fe9a724e79
Charis <bluegrassgal222@gmail.com>
2021-04-30 10:19:36 -0600

    abstract out calculations

--- app.js
+++ app.js
@@ -21,12 +21,34 @@ const errors = {
   futureValue: document.getElementById('future-value-error')
 }
 
-// FUNCTIONS
+// mock state to store values
+let mockState = {
+  pv: 0,        // present value
+  fv: 0,        // future value
+  i: 0,         // interest rate
+  n: 0,         // times compounding per year
+  t: 0,         // time in months
+  pmt: 0        // monthly payment amount
+};
 
+// Util functions and event handlers
 const removeError = field => e => errors[field].classList.add('hidden');
 
 var formatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
 // call with formatter.format(arg)
+
+const handleClear = e => {
+  e.preventDefault();
+
+  Object.values(errors).forEach(err => err.classList.add('hidden'));
+  presentValueInput.value = '';
+  termTextInput.value = '';
+  termSelectInput.value = 'years';
+  interestInput.value = '';
+  compoundsSelect.value = 'monthly';
+  paymentInput.value = '';
+  futureValueInput.value = '';
+};
 
 const resetPage = e => {
   e.preventDefault();
@@ -52,18 +74,17 @@ const toggleTab = e => {
 
 // link inputs and values
 const getValuesFromInput = () => {
-  return {
-    pv: parseFloat(presentValueInput.value),                                // present value
-    fv: parseFloat(futureValueInput.value),                                 // future value
-    i: parseFloat(interestInput.value) / 100,                               // interest rate
-    n: parseFloat(compoundsSelect.value),                                   // times compounding per year
-    t: parseFloat(termTextInput.value) * parseFloat(termSelectInput.value), // time in months
-    pmt: parseFloat(paymentInput.value) || 0                                // monthly payment amount
-  };
+  mockState.pv = parseFloat(presentValueInput.value);
+  mockState.fv = parseFloat(futureValueInput.value);
+  mockState.i = parseFloat(interestInput.value) / 100;
+  mockState.n = parseFloat(compoundsSelect.value);
+  mockState.t = parseFloat(termTextInput.value) * parseFloat(termSelectInput.value);
+  mockState.pmt = parseFloat(paymentInput.value) || 0;
+  return mockState;
 }
 
-// calculations
-const calculatePresentValue = e => {
+// handle submits
+const handlePV = e => {
   e.preventDefault();
 
   // check that the proper field are filled in - display errors
@@ -81,10 +102,41 @@ const calculatePresentValue = e => {
   document.getElementById('charts').classList.add('fade');
 
   // calculate present value
-  let vals = getValuesFromInput();
+  vals = getValuesFromInput();
   vals.pv = 0;
+
+  calculatePV(vals)
+  presentValueInput.value = mockState.pv.toFixed(2); // display present value to user
+}
+
+
+const handleFV = e => {
+  e.preventDefault();
+  // check that the proper field are filled in - display errors
+  let errorsPresent = false;
+  if (presentValueInput.value === '') { errors["presentValue"].classList.remove('hidden'); errorsPresent = true; }
+  if (parseInt(presentValueInput.value) === 0) { errors["presentValue"].classList.remove('hidden'); errorsPresent = true; }
+  if (termTextInput.value === '') { errors["termLength"].classList.remove('hidden'); errorsPresent = true; }
+  if (interestInput.value === '') { errors["interestRate"].classList.remove('hidden'); errorsPresent = true; }
+  if (errorsPresent) return;
+  else Object.values(errors).forEach(err => err.classList.add('hidden'));
+
+  // display graph
+  document.querySelector('form').classList.add('slide');
+  document.getElementById('charts').classList.remove('invisible');
+  document.getElementById('charts').classList.add('fade');
+
+  // calculate value and fill in field
+  vals = getValuesFromInput();
+  vals.fv = 0;
+  claculateFV(vals);
+
+  futureValueInput.value = mockState.fv.toFixed(2); // display future value to user
+};
+
+// calculations and graph creation
+const calculatePV = vals => {
   let dataByMonth = [];
-  // let finalBalance = vals.fv;
 
   for (let count = vals.t; count >= 1; count--){ // iterate through each month, starting with the last
     vals.fv = parseFloat(vals.fv) - parseFloat(vals.pmt); // subtract payment
@@ -104,33 +156,13 @@ const calculatePresentValue = e => {
   for (let i = 1; i < dataByMonth.length; i++){ // make interest cumulative
     dataByMonth[i].interest += dataByMonth[i-1].interest;
   };
-  
-  presentValueInput.value = vals.pv.toFixed(2); // display present value to user
   let principal = vals.pv;
-
-  // trigger graph
+  
   createGraphs(principal, dataByMonth);
-};
+  mockState = vals;
+}
 
-const calculateFutureValue = e => {
-  e.preventDefault();
-  // check that the proper field are filled in - display errors
-  let errorsPresent = false;
-  if (presentValueInput.value === '') { errors["presentValue"].classList.remove('hidden'); errorsPresent = true; }
-  if (parseInt(presentValueInput.value) === 0) { errors["presentValue"].classList.remove('hidden'); errorsPresent = true; }
-  if (termTextInput.value === '') { errors["termLength"].classList.remove('hidden'); errorsPresent = true; }
-  if (interestInput.value === '') { errors["interestRate"].classList.remove('hidden'); errorsPresent = true; }
-  if (errorsPresent) return;
-  else Object.values(errors).forEach(err => err.classList.add('hidden'));
-
-  // display graph
-  document.querySelector('form').classList.add('slide');
-  document.getElementById('charts').classList.remove('invisible');
-  document.getElementById('charts').classList.add('fade');
-
-  // calculate value and fill in field
-  let vals = getValuesFromInput();
-  vals.fv = 0;
+const claculateFV = vals => {
   let principal = vals.pv;
   let dataByMonth = [];
 
@@ -144,36 +176,22 @@ const calculateFutureValue = e => {
     });
     vals.pv = vals.fv; // set the next month's starting balance to this month's ending balance
   }
-
-  futureValueInput.value = vals.fv.toFixed(2); // display future value to user
-
-  // trigger graph
+  
   createGraphs(principal, dataByMonth);
-};
+  mockState = vals;
+}
 
-const handleClear = e => {
-  e.preventDefault();
-
-  Object.values(errors).forEach(err => err.classList.add('hidden'));
-  presentValueInput.value = '';
-  termTextInput.value = '';
-  termSelectInput.value = 'years';
-  interestInput.value = '';
-  compoundsSelect.value = 'monthly';
-  paymentInput.value = '';
-  futureValueInput.value = '';
-};
 
 // add event listeners
 mainHeader.addEventListener('click', resetPage);
 costHeader.addEventListener('click', toggleTab);
 saveHeader.addEventListener('click', toggleTab);
 presentValueInput.addEventListener('input', removeError('presentValue'));
-presentValueGo.addEventListener('click', calculatePresentValue);
+presentValueGo.addEventListener('click', handlePV);
 termTextInput.addEventListener('input', removeError('termLength'));
 interestInput.addEventListener('input', removeError('interestRate'));
 futureValueInput.addEventListener('input', removeError('futureValue'));
-futureValueGo.addEventListener('click', calculateFutureValue);
+futureValueGo.addEventListener('click', handleFV);
 clearBtn.addEventListener('click', handleClear);
 
 
